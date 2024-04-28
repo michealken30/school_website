d25a17486cd1b968f0dbcdacdb00008939d17688
Micheal Oshogwe <kenneth.oshogwe@gmail.com>
2024-04-28 19:33:58 +0100

    school website

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./Components/Hero/Hero";
 import Programs from "./Components/Programs/Programs";
 import Title from "./Components/Title/Title";
 import About from "./Components/About/About";
+import Campus from "./Components/Campus/Campus";
 
 const App = () => {
   return (
@@ -14,6 +15,9 @@ const App = () => {
         <Title subTitle="Our Program" title="What we Offer" />
         <Programs />
         <About />
+        <Title subTitle="Gallery" title="Campus Photo" />
+        <Campus />
+        <Title subTitle="TESTIMONIALS" title="What Students Says" />
       </div>
     </div>
   );
---
+++ src/Components/Campus/Campus.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./campus.css";
+import gallaery_1 from "../../assets/gallery-1.png";
+import gallaery_2 from "../../assets/gallery-2.png";
+import gallaery_3 from "../../assets/gallery-3.png";
+import gallaery_4 from "../../assets/gallery-4.png";
+import white_arrow from "../../assets/white-arrow.png";
+
+const Campus = () => {
+  return (
+    <div className="campus">
+      <div className="gallery">
+        <img src={gallaery_1} alt="" />
+        <img src={gallaery_2} alt="" />
+        <img src={gallaery_3} alt="" />
+        <img src={gallaery_4} alt="" />
+      </div>
+      <button className="btn dark-btn">
+        See more here <img src={white_arrow} alt="" />
+      </button>
+    </div>
+  );
+};
+
+export default Campus;
